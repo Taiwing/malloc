055ffe45873d768c02c38197c656ed25ddaae368
Yoann FOREAU <yforeau@e1r1p1.clusters.42paris.fr>
2022-08-25 18:49:07 +0200

    feat: create new zone and split blocks

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:10:38 by yforeau           #+#    #+#             */
-/*   Updated: 2022/08/23 16:11:46 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/08/25 17:52:34 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,83 @@
 
 # include <stdlib.h>
 
+enum e_block_type	{
+	E_INVALID_BLOCK,
+	E_TINY_BLOCK,
+	E_SMALL_BLOCK,
+	E_LARGE_BLOCK,
+};
+
+# define	TINY_BLOCK_SIZE_MIN		1		// min size for a tiny block
+# define	SMALL_BLOCK_SIZE_MIN	512		// min size for a small block
+# define	LARGE_BLOCK_SIZE_MIN	4096	// min size for a large block
+
+# define	TINY_BLOCK_SIZE_MAX		(SMALL_BLOCK_SIZE_MIN - 1)
+# define	SMALL_BLOCK_SIZE_MAX	(LARGE_BLOCK_SIZE_MIN - 1)
+
+# define	ZONE_LENGTH				128		// blocks per tiny/small zone
+
+/*
+** Memory Block: Individually allocated memory block or free memory
+**
+** type: size type of the memory block (tiny, small or large)
+** size: size of the memory block (not counting this structure)
+** free: boolean, 1 if the block is free 0 otherwise
+** prev: previous block
+** next: next block
+*/
+typedef struct				s_memory_block
+{
+	enum e_block_type		type;
+	size_t					size;
+	int						free;
+	struct s_memory_block	*prev;
+	struct s_memory_block	*next;
+}							t_memory_block __attribute__ ((aligned(8)));
+
+/*
+** Memory Zone: mmap allocated memory zone
+**
+** type: what type of memory blocks it holds (tiny, small or large)
+** size: total size of the zone counting this structure (mmap parameter)
+** blocks: memory blocks list
+** prev: previous memory zone
+** next: next memory zone
+*/
+typedef struct				s_memory_zone
+{
+	enum e_block_type		type;
+	size_t					size;
+	t_memory_block			*blocks;
+	struct s_memory_zone	*prev;
+	struct s_memory_zone	*next;
+}							t_memory_zone __attribute__ ((aligned(8)));
+
 void	free(void *ptr);
 void	*malloc(size_t size);
 void	*realloc(void *ptr, size_t size);
+
+# define	TINY_ZONE_SIZE_MIN	(\
+	sizeof(t_memory_zone) +\
+	(TINY_BLOCK_SIZE_MAX + sizeof(t_memory_block)) * ZONE_LENGTH\
+)
+
+# define	SMALL_ZONE_SIZE_MIN	(\
+	sizeof(t_memory_zone) +\
+	(SMALL_BLOCK_SIZE_MAX + sizeof(t_memory_block)) * ZONE_LENGTH\
+)
+
+// Return a multiple of 8 from the given value (by increasing it if needed)
+# define	ALIGN_EIGHT(VALUE)	(((VALUE + 7) >> 3) << 3)
+
+// Mininum space a free tiny block must contain after a split
+# define	TINY_FREE_BLOCK_SIZE_MIN	(\
+	TINY_BLOCK_SIZE_MIN + sizeof(t_memory_block)\
+)
+
+// Mininum space a free small block must contain after a split
+# define	SMALL_FREE_BLOCK_SIZE_MIN	(\
+	SMALL_BLOCK_SIZE_MIN + sizeof(t_memory_block)\
+)
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -6,39 +6,188 @@
 /*   By: yforeau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:05:33 by yforeau           #+#    #+#             */
-/*   Updated: 2022/08/23 19:27:52 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/08/25 18:47:50 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
+#include <unistd.h>
 #include <sys/mman.h>
 
 //TEMP
-#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 //TEMP
 
-void	free(void *ptr)
+t_memory_zone	*zones = NULL;
+
+enum e_block_type	block_type_from_size(size_t size)
 {
-	//TODO
-	(void)ptr;
+	if (size >= LARGE_BLOCK_SIZE_MIN)
+		return (E_LARGE_BLOCK);
+	else if (size >= SMALL_BLOCK_SIZE_MIN)
+		return (E_SMALL_BLOCK);
+	else if (size >= TINY_BLOCK_SIZE_MIN)
+		return (E_TINY_BLOCK);
+	return (E_INVALID_BLOCK);
+}
+
+/*
+** Utility boolean function returning 1 if the given block is splitable after
+** the size is allocated. Does not check the errors regarding the size or the
+** type of the block. It will simply return 0 in these cases.
+*/
+int			is_splitable_block(t_memory_block *block, size_t size)
+{
+	if (block->size == size)
+		return (0);
+	switch (block->type)
+	{
+		case E_TINY_BLOCK:
+			return (block->size >= (ALIGN_EIGHT(size)
+				+ TINY_FREE_BLOCK_SIZE_MIN));
+			break;
+		case E_SMALL_BLOCK:
+			return (block->size >= (ALIGN_EIGHT(size)
+				+ SMALL_FREE_BLOCK_SIZE_MIN));
+			break;
+		default:
+			return (0);
+			break;
+	}
+}
+
+/*
+** Looks for an available block of free memory in the zones list depending on
+** the requested size. Invalid or large allocations always return NULL,
+** otherwise it returns a pointer to the free block if there is one available.
+*/
+t_memory_block	*get_free_block(t_memory_zone *zones, size_t size)
+{
+	enum e_block_type	type;
+	t_memory_block		*free_block = NULL;
+
+	type = block_type_from_size(size);
+	if (!zones || type == E_LARGE_BLOCK || type == E_INVALID_BLOCK)
+		return (free_block);
+	for (; zones; zones = zones->next)
+	{
+		if (zones->type != type)
+			continue;
+		for (t_memory_block *block = zones->blocks; block; block = block->next)
+		{
+			if (!block->free)
+				continue;
+			else if (block->size == size)
+				return (block);
+			else if (block->size > size)
+				free_block = !free_block || free_block->size > block->size ?
+					block : free_block;
+		}
+	}
+	return (free_block);
+}
+
+/*
+** Set a given free block as allocated.
+*/
+void		allocate_free_block(t_memory_block *block, size_t size)
+{
+	t_memory_block	new_block = { 0 };
+
+	block->free = 0;
+	if (is_splitable_block(block, size))
+	{
+		new_block.free = 1;
+		new_block.size = block->size - ALIGN_EIGHT(size) - sizeof(new_block);
+		new_block.prev = block;
+		new_block.next = block->next;
+		block->size = ALIGN_EIGHT(size);
+		block->next = block + block->size;
+		memcpy(block->next, &new_block, sizeof(new_block));
+	}
+}
+
+void		free(void *ptr)
+{
+	t_memory_block	*block;
+
+	if (!ptr)
+		return;
+	block = ptr - sizeof(t_memory_block);
+	block->free = 1;
+	//TODO: defragment memory by merging empty adjacent blocks
 	write(1, "free!\n", 6); //TEMP
 }
 
-void	*malloc(size_t size)
+size_t			get_zone_size(enum e_block_type type, size_t block_size)
 {
-	char	*mem;
-	write(1, "malloc!\n", 8); //TEMP
+	size_t	zone_size = 0, page_size = getpagesize();
 
-	mem = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-	//TEMP
-	if (mem == MAP_FAILED)
+	switch (type)
 	{
-		write(1, "MAP_FAILED\n", 11);
-		perror("mmap");
+		case E_INVALID_BLOCK:
+			return (zone_size);
+			break;
+		case E_TINY_BLOCK:
+			zone_size = TINY_ZONE_SIZE_MIN;
+			break ;
+		case E_SMALL_BLOCK:
+			zone_size = SMALL_ZONE_SIZE_MIN;
+			break ;
+		case E_LARGE_BLOCK:
+			zone_size = block_size + sizeof(t_memory_zone)
+				+ sizeof(t_memory_block);
+			break ;
 	}
-	//TEMP
-	return (mem == MAP_FAILED ? NULL : mem);
+	if (zone_size % page_size)
+		zone_size += page_size - (zone_size % page_size);
+	return (zone_size);
+}
+
+t_memory_zone	*push_new_zone(t_memory_zone **zones, size_t size)
+{
+	t_memory_zone		*zone = NULL;
+	enum e_block_type	type = block_type_from_size(size);
+	size_t				zone_size = get_zone_size(type, size);
+	t_memory_block		first_block = { 0 };
+
+	if ((zone = mmap(NULL, zone_size, PROT_READ | PROT_WRITE | PROT_EXEC,
+		MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED || !zone)
+		return (NULL);
+	zone->type = type;
+	zone->size = zone_size;
+	zone->blocks = (void *)zone + sizeof(t_memory_zone);
+	zone->prev = NULL;
+	zone->next = NULL;
+	first_block.type = type;
+	first_block.size = zone_size - sizeof(t_memory_zone);
+	first_block.free = 1;
+	memcpy(zone->blocks, &first_block, sizeof(first_block));
+	while (*zones && (*zones)->next)
+		zones = &((*zones)->next);
+	*zones = zone;
+	return (zone);
+}
+
+void		*malloc(size_t size)
+{
+	t_memory_zone	*zone = NULL;
+	t_memory_block	*block = NULL;
+
+	write(1, "malloc!\n", 8); //TEMP
+	if (!size)
+		return (NULL);
+	else if ((block = get_free_block(zones, size)))
+		allocate_free_block(block, size);
+	else if (!(zone = push_new_zone(&zones, size)))
+		return (NULL);
+	else
+	{
+		block = zone->blocks;
+		allocate_free_block(block, size);
+	}
+	return ((void *)block + sizeof(t_memory_block));
 }
 
 void	*realloc(void *ptr, size_t size)
